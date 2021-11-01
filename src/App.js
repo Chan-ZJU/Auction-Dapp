a6634cbc00c0e706b3c3a62fd6a60d7342ad76b5
Chan-ZJU <1072122585@qq.com>
2021-11-01 09:15:53 +0800

    some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
-import Home from './ui/ui'
-import "./App.css"
+import Home from './ui/ui';
+import "./App.css";
+const pinataApiKey = "1e57bed1d89a30e5a5a7";
+const pinataSecretApiKey = "81e1b6cf23942787bf6a8b55cd83ac96ab46a37600e48b146d0a2a55d0f4f79a";
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
 
 let web3 = require('./util/initWeb3')
 let auctionInstance = require('./eth/auction')
@@ -15,6 +20,26 @@ class App extends Component{
 
     }
   }
+
+
+
+  pinFileToIPFS = async () => {
+  const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+  let data = new FormData();
+  data.append("file", fs.createReadStream("./pathtoyourfile.png"));
+  const res = await axios.post(url, data, {
+    maxContentLength: "Infinity", 
+    headers: {
+      "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+      "pinata_api_key": pinataApiKey, 
+      "pinata_secret_api_key": pinataSecretApiKey,
+    },
+  });
+  console.log(res.data);
+  };
+  
+
+
 
   componentDidMount() {
   }
